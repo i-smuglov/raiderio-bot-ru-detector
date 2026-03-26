53fd4311b62673a489ba28759a3675f2ea073c48
i-smuglov <ysmuglov@gmail.com>
2026-03-26 23:44:38 +0100

    rm GuildMembers

--- src/index.js
+++ src/index.js
@@ -44,7 +44,6 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
   ],
 });
 
